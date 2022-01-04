bfc9a30334aa6629943cd13742620a313faf5f83
guoyujian <guoyujian237@qq.com>
2022-01-04 22:49:38 +0800

    labuladong: 链表递归

---
+++ labuladong/92. 反转链表 II/Solution.java
@@ -0,0 +1,45 @@
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // base case
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+        // 前进到反转的起点触发 base case
+        head.next = reverseBetween(head.next, left - 1, right - 1);
+        return head;
+    }
+
+    ListNode successor = null; // 后驱节点
+    // 反转以 head 为起点的 n 个节点，返回新的头结点
+    ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            // 记录第 n + 1 个节点
+            successor = head.next;
+            return head;
+        }
+        // 以 head.next 为起点，需要反转前 n - 1 个节点
+        ListNode last = reverseN(head.next, n - 1);
+
+        head.next.next = head;
+        // 让反转之后的 head 节点和后面的节点连起来
+        head.next = successor;
+        return last;
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
